40b9e2e24fc06dc61c07b4acaf4e15ef268e2849
Archan Ray <archan@dido.cs.umass.edu>
2019-04-02 14:24:40 -0400

    new kernel definitions

--- GVAE.py
+++ GVAE.py
@@ -43,38 +43,69 @@ class VGAE_GCN_IP(nn.Module):
 	'''
 	def __init__(self, input_size):
 		super(GVAE, self).__init__()
-                self.gc1 = GC(input_feat_dim, hidden_dim1, dropout, act=F.relu)
-                self.gc2 = GC(hidden_dim1, hidden_dim2, dropout, act=lambda x: x)
-                self.gc3 = GC(hidden_dim1, hidden_dim2, dropout, act=lambda x: x)
-                self.dc = InnerProductDecoder(dropout, act=lambda x: x)
+        self.gc1 = GC(input_feat_dim, hidden_dim1, dropout, act=F.relu)
+        self.gc2 = GC(hidden_dim1, hidden_dim2, dropout, act=lambda x: x)
+        self.gc3 = GC(hidden_dim1, hidden_dim2, dropout, act=lambda x: x)
+        self.dc = InnerProductDecoder(dropout, act=lambda x: x)
 
-        def encode(self, x, adj):
-                hidden1 = self.gc1(x, adj)
-                return self.gc2(hidden1, adj), self.gc3(hidden1, adj)
+    def encode(self, x, adj):
+        hidden1 = self.gc1(x, adj)
+        return self.gc2(hidden1, adj), self.gc3(hidden1, adj)
 
-        def reparameterize(self, mu, logvar):
-                if self.training:
-                        std = torch.exp(0.5*logvar)
-                        eps = torch.randn_like(std)
-                        return eps.mul(std).add_(mu)
-                else:
-                        return mu
+    def reparameterize(self, mu, logvar):
+        if self.training:
+                std = torch.exp(0.5*logvar)
+                eps = torch.randn_like(std)
+                return eps.mul(std).add_(mu)
+        else:
+                return mu
 
-        def forward(self, x, adj):
-                mu, logvar = self.encode(x, adj)
-                z = self.reparameterize(mu, logvar)
-                return self.dc(z), mu, logvar
+    def forward(self, x, adj):
+        mu, logvar = self.encode(x, adj)
+        z = self.reparameterize(mu, logvar)
+        return self.dc(z), mu, logvar
 
 class InnerProductDecoder(nn.Module):
-        '''
-        Decoder for using inner product for prediction
-        '''
-        def __init__(self, dropout, act=torch.sigmoid):
-                super(InnerProductDecoder, self).__init__()
-                self.dropout = dropout
-                self.act = act
+    '''
+    Decoder for using inner product for prediction
+    '''
+    def __init__(self, dropout, act=torch.sigmoid):
+        super(InnerProductDecoder, self).__init__()
+        self.dropout = dropout
+        self.act = act
 
-        def forward(self, z):
-                z = F.dropout(z, self.dropout, training=self.training)
-                adj = self.act(torch.mm(z, z.t()))
-                return adj
+    def forward(self, z):
+        z = F.dropout(z, self.dropout, training=self.training)
+        adj = self.act(torch.mm(z, z.t()))
+        return adj
+
+class RBFKernelDecoder(nn.Module)
+	'''
+    Decoder for using inner product for prediction
+    '''
+    def __init__(self, dropout, act=torch.sigmoid):
+        super(InnerProductDecoder, self).__init__()
+        self.dropout = dropout
+        self.act = act
+
+    def forward(self, z):
+        z = F.dropout(z, self.dropout, training=self.training)
+        # adj = exp(norm(z,z)^2/2*sigma^2)
+        #adj = self.act(torch.mm(z, z.t()))
+        return adj
+
+
+class PolyKernelDecoder(nn.Module):
+	'''
+    Decoder for using inner product for prediction
+    '''
+    def __init__(self, dropout, act=torch.sigmoid):
+        super(InnerProductDecoder, self).__init__()
+        self.dropout = dropout
+        self.act = act
+
+    def forward(self, z):
+        z = F.dropout(z, self.dropout, training=self.training)
+        # adj = (torch.mm(z, z.t())+c)^d
+        #adj = self.act(torch.mm(z, z.t()))
+        return adj
